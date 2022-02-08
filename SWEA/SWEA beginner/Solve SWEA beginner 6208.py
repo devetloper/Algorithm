dbd8501ae43c284b18e7ccb1ab5df0bc344ce8dd
devetloper <simpson9713@naver.com>
2022-02-08 20:10:20 +0900

    Solve SWEA beginner 6208

---
+++ SWEA/SWEA beginner/Solve SWEA beginner 6208.py
@@ -0,0 +1,13 @@
+class Korean():
+	
+	def __init__(self,nationality):
+		self.nationality = nationality
+		
+	def printNationality(self):
+		print(self.nationality)
+
+a = Korean("대한민국")
+a.printNationality()
+a.printNationality()
+
+
