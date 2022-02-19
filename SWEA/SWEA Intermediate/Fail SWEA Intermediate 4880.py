a14d6adcd41b6cc87e76dc58c3bb73ef426f6456
devetloper <simpson9713@naver.com>
2022-02-19 22:06:49 +0900

    Fail SWEA Intermediate 4880 #3

--- SWEA/SWEA Intermediate/Fail SWEA Intermediate 4880.py
+++ SWEA/SWEA Intermediate/Fail SWEA Intermediate 4880.py
@@ -1,7 +1,8 @@
-#/// have to convert [1,2,3,5,6] to [[1,2],3,[5,6]]
+# // have to put tor_list in half function #
 ### grouping student ###
 tor_list = []
 def half(i,j):
+    tor_list.clear()
     if i == j:
         tor_list.append(j)
         return tor_list
@@ -39,16 +40,37 @@ cards = input().split(" ")
 card_dict = {}
 card_dict = {i : int(cards[i-1]) for i in range(1,n+1)}
 
-###
+### make mathcing function ###
 
 
 def match(gr_list):
-    for group in gr_list:
+    if len(gr_list) == 2:
+        return rcp(gr_list)
+    
+    else:
+        for group in gr_list:
+            if type(group) == list:
+                idx = gr_list.index(group)
+                gr_list.remove(group)
+                gr_list.insert(idx,rcp(group))
+        return match(regroup(gr_list))
+
+### make regroup function ### ex) convert [1,3,5,6] to [[1,3],[5,6]]
+
+def regroup(re_list):
+    len_li =len(re_list)
+    group_li = half(1,len(re_list))
+    for group in group_li:
+
         if type(group) == list:
-            idx = gr_list.index(group)
-            gr_list.remove(group)
-            gr_list.insert(idx,rcp(group))
-    return gr_list
+            re_list.append([re_list[group[0]-1],re_list[group[1]-1]])
+        
+        else:
+            re_list.append(re_list[group-1])
+
+
+    re_list = re_list[len_li:]
+    return re_list
 
 groups = half(1,n)
 
