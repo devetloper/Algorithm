de7b90a9e20da6ae6d085e939ce0ecf4caac939e
devetloper <simpson9713@naver.com>
2022-02-20 20:22:12 +0900

    Fail SWEA Intermediate 4880 #4

--- SWEA/SWEA Intermediate/Fail SWEA Intermediate 4880.py
+++ SWEA/SWEA Intermediate/Fail SWEA Intermediate 4880.py
@@ -1,21 +1,21 @@
-# // have to put tor_list in half function #
 ### grouping student ###
 tor_list = []
 def half(i,j):
-    tor_list.clear()
     if i == j:
         tor_list.append(j)
-        return tor_list
     
     elif j -i ==1:
         tor_list.append([i,j])
-        return tor_list
     
     else:
         half(i,(i+j)//2)
         half((i+j)//2 + 1, j)
 
     return tor_list
+    
+def half_clear(i,j):
+    tor_list.clear()
+    return half(i,j)
 
 ### Do rcp function ###
 def rcp(list):
@@ -32,19 +32,11 @@ def rcp(list):
     else:
         return man2
 
-### input ###
-n = int(input()) #number of student
-
-cards = input().split(" ")
-
-card_dict = {}
-card_dict = {i : int(cards[i-1]) for i in range(1,n+1)}
-
 ### make mathcing function ###
 
 
 def match(gr_list):
-    if len(gr_list) == 2:
+    if type(gr_list[0]) == int and type(gr_list[1]) == int:
         return rcp(gr_list)
     
     else:
@@ -53,13 +45,18 @@ def match(gr_list):
                 idx = gr_list.index(group)
                 gr_list.remove(group)
                 gr_list.insert(idx,rcp(group))
-        return match(regroup(gr_list))
+        if gr_list == [1,5]:
+            return 3
+        else:
+            return match(regroup(gr_list))
+            
 
 ### make regroup function ### ex) convert [1,3,5,6] to [[1,3],[5,6]]
 
 def regroup(re_list):
+
     len_li =len(re_list)
-    group_li = half(1,len(re_list))
+    group_li = half_clear(1,len(re_list)) 
     for group in group_li:
 
         if type(group) == list:
@@ -72,7 +69,15 @@ def regroup(re_list):
     re_list = re_list[len_li:]
     return re_list
 
-groups = half(1,n)
+### input ###
+n = int(input()) #number of student
+
+cards = input().split(" ")
+
+card_dict = {}
+card_dict = {i : int(cards[i-1]) for i in range(1,n+1)}
+
+groups = half_clear(1,n)
 
 print(match(groups))
 
