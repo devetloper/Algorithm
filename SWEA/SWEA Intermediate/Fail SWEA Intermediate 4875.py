dfbccbacd7effd1ff2e90b8669ca0979cd58ea37
devetloper <simpson9713@naver.com>
2022-03-03 22:44:29 +0900

    Fail SWEA Intermediate 4875 #3

--- SWEA/SWEA Intermediate/Fail SWEA Intermediate 4875.py
+++ SWEA/SWEA Intermediate/Fail SWEA Intermediate 4875.py
@@ -18,27 +18,12 @@ for i in range(n):
 stack = []
 
 def search(x,y):
-	try:
-		if maze[x-1][y] or maze[x][y+1] or maze[x+1][y] or maze[x][y-1] == 3:
-			return 1
+	pass # if 0 or 3 in up,right,down,left return True
+
+def move(x,y):
+	if search(x,y):
+		pass 	# move and put in stack
 		
-		elif maze[x-1][y] == 0:
-			return [x-1,y]
-		
-		elif maze[x][y+1] == 0:
-			return [x,y+1]
-		
-		elif maze[x+1][y] == 0:
-			return [x+1,y]
-		
-		elif maze[x][y-1] == 0:
-			return [x,y-1]
-		
-		else:
-			return False
-			
-	except IndexError:
-		print(5)
 					
 start_x, start_y = start
 print(start_x,start_y)
