402afbf42f5503813cd5495a311c6d1527b92d99
devetloper <simpson9713@naver.com>
2022-03-07 06:05:31 +0900

    Solve SWEA Intermediate 5108

---
+++ SWEA/SWEA Intermediate/Solve SWEA Intermediate 5108.py
@@ -0,0 +1,11 @@
+T = int(input())
+for test_case in range(1, T + 1):
+
+    n, m , l = map(int,input().split())
+    num_list = list(map(int,input().split()))
+
+    for _ in range(m):
+        idx, num = map(int,input().split())
+        num_list.insert(idx,num)
+
+    print(f"#{test_case}",num_list[l]) 
