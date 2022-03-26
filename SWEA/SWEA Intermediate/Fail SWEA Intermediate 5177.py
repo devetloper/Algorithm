2b75a301e2a54dab77a657dbe206196ec0ef017a
devetloper <simpson9713@naver.com>
2022-03-26 13:20:48 +0900

    Fail SWEA Intermediate 5177 #3

--- SWEA/SWEA Intermediate/Fail SWEA Intermediate 5177.py
+++ SWEA/SWEA Intermediate/Fail SWEA Intermediate 5177.py
@@ -1,5 +1,4 @@
-### make heap tree , Have to calculate sum of parents
-
+# Fail 3/9 test case
 def min_heap(arr):
     h = 1
     while len(arr) > 2**h -1:
@@ -14,18 +13,33 @@ def min_heap(arr):
                 temp = arr[i] 
                 arr[i] = arr[2*i + 2]
                 arr[2*i + 2] = temp
+
         elif (2*i + 1) < len(arr):
             if arr[i] > arr[2*i + 1]:
                 temp = arr[i] 
                 arr[i] = arr[2*i + 1]
                 arr[2*i + 1] = temp
 
-n = int(input())
-nums = list(map(int,input().split()))
-heap_nums = []
+def sum_parent(arr):
+    par = int((len(arr)-1)/2)
+    total = 0
+    while par >= 0:
+        total += arr[par]
+        if par == 0:
+            break
+        par = int((par-1)/2)
+    return total
 
-for num in nums:
-    heap_nums.append(num)
-    min_heap(heap_nums)
+T = int(input())
+for test_case in range(1, T + 1):
+    n = int(input())
+    nums = list(map(int,input().split()))
+    heap_nums = []
 
-print(heap_nums)
+    for num in nums:
+        heap_nums.append(num)
+        min_heap(heap_nums)
+
+    
+    ans = sum_parent(heap_nums)
+    print(f"#{test_case}",ans)
