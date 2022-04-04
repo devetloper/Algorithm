8a23dfc8ae152fa74a02a5c5413e66c349983d2c
devetloper <simpson9713@naver.com>
2022-04-04 10:59:34 +0900

    Fail SWEA Intermediate 4881 #2

--- SWEA/SWEA Intermediate/Fail SWEA Intermediate 4881.py
+++ SWEA/SWEA Intermediate/Fail SWEA Intermediate 4881.py
@@ -1,37 +1,22 @@
-#Run Time Error, Not using backtracking (5/10)
-def permute(arr): #get help
-    result = [arr[:]]
-    c = [0] * len(arr)
-    i = 0
-    while i < len(arr):
-        if c[i] < i:
-            if i % 2 == 0:
-                arr[0], arr[i] = arr[i], arr[0]
-            else:
-                arr[c[i]], arr[i] = arr[i], arr[c[i]]
-            result.append(arr[:])
-            c[i] += 1
-            i = 0
-        else:
-            c[i] = 0
-            i += 1
-    return result
+def backtracking(r= 0,v=[],s=0):
+    global min
+    if r == 3:
+        if s < min:
+            min = s
+        return
+    for i in range(n):
+        s += numbers[r][i]
+        v.append(i)
+        r += 1
+        backtracking(r,v,s)
 
-T = int(input())
-for test_case in range(1, T + 1):
-    n = int(input())
-    numbers =[]
-    for _ in range(n):
-        row = list(map(int,input().split()))
-        numbers.append(row)
 
-    per = permute(list(range(n)))
+n = int(input())
+numbers =[]
+for _ in range(n):
+    row = list(map(int,input().split()))
+    numbers.append(row)
 
-    min = 99**99
-    for col in per:
-        total = 0
-        for i in range(n):
-            total += numbers[i][col[i]-1]
-        if total < min:
-            min = total
-    print(f"#{test_case}",min)
+min=99**99
+s = backtracking(0,[],0)
+print(min)
